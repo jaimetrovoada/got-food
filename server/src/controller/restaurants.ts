4b8399f7107e99f29e5875f9b4feed58464f76f3
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-19 21:01:15 +0800

    add basic file upload

--- server/src/controller/restaurants.ts
+++ server/src/controller/restaurants.ts
@@ -1,7 +1,17 @@
 import express from "express";
 import models from "../model";
+import multer from "multer";
 
 const router = express.Router();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "." + file.originalname.split(".").pop());
+  },
+});
+const upload = multer({ storage: storage });
 
 router.get("/", async (req, res) => {
   try {
@@ -61,34 +71,39 @@ router.get("/:id/menu", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", upload.single("logo"), async (req, res) => {
   try {
-    const restaurant = await models.Restaurant.create({
+    const _restaurant = {
       name: req.body.name,
       description: req.body.description,
       address: req.body.address,
-      menu: req.body.menu,
-      owner: req.body.owner,
-    });
+      menuItems: [],
+      // owner: req.body.owner,
+      image: req.file.destination + "/" + req.file.filename,
+    };
 
-    // TODO: handle file upload
+    const restaurant = new models.Restaurant(_restaurant);
 
-    res.json(restaurant);
+    res.status(201).json(restaurant);
   } catch (err) {
     res.status(500).json({ message: err.message });
     console.log({ err });
   }
 });
 
-router.post("/:id/menu", async (req, res) => {
+router.post("/:id/menu", upload.single("food"), async (req, res) => {
   try {
-    const menuItem = new models.Menu({
+    const _menuItem = {
       restaurant: req.params.id,
       name: req.body.name,
       description: req.body.description,
       price: req.body.price,
       category: req.body.category,
-    });
+      image: req.file.destination + "/" + req.file.filename,
+    };
+
+    const menuItem = new models.Menu(_menuItem);
+
     await menuItem.save();
     res.status(201).json(menuItem);
   } catch (err) {
@@ -97,6 +112,8 @@ router.post("/:id/menu", async (req, res) => {
   }
 });
 
-// TODO: add put request for updating restaurant info
-
 export default router;
+
+// TODO: allow multiple file upload
+// TODO: add images to different folders according to restaurant
+// TODO: add put request for updating restaurant info
