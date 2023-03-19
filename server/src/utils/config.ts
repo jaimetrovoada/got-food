a8f53987a0d6865e95594fb71db048ddd78f516f
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-19 17:58:26 +0800

    set test to use different db

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -3,7 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-    MONGODB_URI: process.env.MONGODB_URI,
-}
+  MONGODB_URI:
+    process.env.NODE_ENV === "test"
+      ? process.env.TEST_MONGODB_URI
+      : process.env.MONGODB_URI,
+};
 
 export default config;
