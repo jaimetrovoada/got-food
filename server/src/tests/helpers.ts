a27422064dd21bd1065eef4fa9fd7e2b1d59f695
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-19 17:53:33 +0800

    update initDb test helper

--- server/src/tests/helpers.ts
+++ server/src/tests/helpers.ts
@@ -1,5 +1,6 @@
-import Restaurant from "../model/restaurant";
-import User from "../model/user";
+import Menu from "../model/menu";
+import models from "../model";
+import mongoose from "mongoose";
 
 export const users = [
   {
@@ -33,26 +34,56 @@ export const restaurants = [
     name: "restaurant1",
     description: "description1",
     address: "address1",
-    menu: [
-      {
-        name: "menu1",
-        price: 10,
-      },
-    ],
-    owner: "user1",
   },
 ];
 
+export const menuItems = [
+  {
+    name: "menuItem1",
+    description: "description1",
+    price: 10,
+    category: "drinks",
+    image: "image1",
+  },
+  {
+    name: "menuItem2",
+    description: "description1",
+    price: 10,
+    category: "dessert",
+    image: "image1",
+  },
+  {
+    name: "menuItem3",
+    description: "description1",
+    price: 10,
+    category: "drinks",
+    image: "image1",
+  },
+];
 export const initDb = async () => {
-  await Restaurant.deleteMany({});
-  await User.deleteMany({});
+  await models.Restaurant.deleteMany({});
+  await models.User.deleteMany({});
 
-  const usersObj = users.map((user) => new User(user));
+  const usersObj = users.map((user) => new models.User(user));
   const usersPromiseArr = usersObj.map((user) => user.save());
-
-  const restObj = restaurants.map((rest) => new Restaurant(rest));
-  const restPromiseArr = restObj.map((rest) => rest.save());
-
   await Promise.all(usersPromiseArr);
+  const _users = await models.User.find({});
+
+  const restObj = restaurants.map(
+    (rest) => new models.Restaurant({ ...rest, owner: _users[0]._id })
+  );
+  const restPromiseArr = restObj.map((rest) => {
+    rest.save();
+  });
   await Promise.all(restPromiseArr);
+  const _restaurants = await models.Restaurant.find({});
+
+  const menuItemsObj = menuItems.map(
+    (menuItem) => new Menu({ ...menuItem, restaurant: _restaurants[0]._id })
+  );
+  const menuItemsPromiseArr = menuItemsObj.map((menuItem) => {
+    menuItem.save();
+  });
+
+  await Promise.all(menuItemsPromiseArr);
 };
