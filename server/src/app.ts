726a13c5a43bf9e36533976cd3d7f55b03a427f2
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-27 21:17:05 +0800

    add ping route

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,19 +2,29 @@ import express from "express";
 import mongoose from "mongoose";
 import config from "./utils/config";
 import routes from "./controller";
-import middleware from "./utils/middleware";
 import morgan from "morgan";
+import logger from "./utils/logger";
 
 const app = express();
 
-mongoose.connect(config.MONGODB_URI);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((err) => {
+    logger.error("Could not connect to MongoDB", err);
+  });
 
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(middleware.userExtractor);
 
 app.use("/restaurants", routes.restaurants);
 app.use("/users", routes.users);
+
+app.use("/ping", (req, res) => {
+  res.send("pong");
+});
 //console.log({ dir: path.join(__dirname, "../public") });
 app.use("/", express.static("public"));
 
