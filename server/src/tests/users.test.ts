6db84d5886056773935e8b098dc5105acbd58539
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-28 11:31:06 +0800

    refactor users tests

--- server/src/tests/users.test.ts
+++ server/src/tests/users.test.ts
@@ -4,69 +4,81 @@ import testHelpers from "../utils/testHelpers";
 
 const TIMEOUT = 100_000;
 
-// TODO: refactor tests
-
 const api = supertest(app);
 describe("users", () => {
   beforeEach(async () => {
-    testHelpers.initDb();
+    testHelpers.emptyDb();
   }, TIMEOUT);
 
-  test("can get users", async () => {
-    const res = await api.get("/users");
+  test(
+    "can register customer user",
+    async () => {
+      const user = {
+        name: "user1",
+        email: "wario@gmail.com",
+        password: "abc12345",
+        role: "customer",
+      };
+      const res = await api.post("/users/register").send(user);
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe(user.name);
+      expect(res.body.email).toBe(user.email);
+      expect(res.body.role).toBe(user.role);
+      expect(res.body.passwordHash).not.toBeDefined();
+      expect(res.body.password).not.toBeDefined();
+    },
+    TIMEOUT
+  );
 
-    expect(res.status).toBe(200);
-    expect(res.body.length).toBe(4);
+  test("error if role is unsupported", async () => {
+    const user = {
+      name: "user1",
+      email: "wario@gmail.com",
+      password: "abc12345",
+      role: "admin",
+    };
 
-    res.body?.forEach((user: any) => {
-      expect(user).toHaveProperty("id");
-      expect(user).toHaveProperty("name");
-      expect(user).toHaveProperty("email");
-      expect(user).toHaveProperty("role");
-      expect(user.role).not.toBe("admin");
-      expect(user).not.toHaveProperty("password");
-    });
+    const res = await api.post("/users/register").send(user);
+
+    expect(res.body.error[0]).toContain(
+      "Expected 'customer' | 'business', received 'admin'"
+    );
+    expect(res.status).toBe(400);
   });
 
-  test("can add users", async () => {
+  test("can handle multiple zod validation errors", async () => {
     const user = {
-      name: "test",
-      email: "test",
-      password: "test",
+      name: "user1",
+      email: "wario@gmail.com",
+      password: "abc123",
+      role: "admin",
+    };
+
+    const res = await api.post("/users/register").send(user);
+
+    expect(res.body.error[0]).toContain("Password");
+    expect(res.body.error[1]).toContain(
+      "Expected 'customer' | 'business', received 'admin'"
+    );
+    expect(res.status).toBe(400);
+  });
+
+  test("can login", async () => {
+    const user = {
+      name: "user1",
+      email: "wario@gmail.com",
+      password: "abc12345",
       role: "customer",
     };
-    await api
-      .post("/users")
-      .send(user)
-      .expect("Content-Type", /application\/json/)
-      .expect(201)
-      .expect((res) => {
-        expect(res.body.name).toBe(user.name);
-        expect(res.body.email).toBe(user.email);
-        expect(res.body.role).toBe(user.role);
-        expect(res.body).not.toHaveProperty("password");
-        expect(res.body).not.toHaveProperty("restaurants");
-      });
-  });
 
-  test("add business user", async () => {
-    const user = {
-      name: "test",
-      email: "test",
-      password: "test",
-      role: "business",
-    };
-    await api
-      .post("/users")
-      .send(user)
-      .expect("Content-Type", /application\/json/)
-      .expect(201)
-      .expect((res) => {
-        expect(res.body.name).toBe(user.name);
-        expect(res.body.email).toBe(user.email);
-        expect(res.body.role).toBe(user.role);
-        expect(res.body).not.toHaveProperty("password");
-        expect(res.body).toHaveProperty("restaurants");
-      });
+    await api.post("/users/register").send(user);
+
+    const res = await api.post("/users/login").send({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(res.status).toBe(200);
+    expect(res.body.token).toBeDefined();
   });
 });
