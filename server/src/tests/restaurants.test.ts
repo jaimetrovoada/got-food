5ef4555b11db2f59f1271dca69e86a4bbca305e1
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-28 12:58:29 +0800

    refactor restaurants test

--- server/src/tests/restaurants.test.ts
+++ server/src/tests/restaurants.test.ts
@@ -8,38 +8,45 @@ const TIMEOUT = 100_000;
 const api = supertest(app);
 
 beforeEach(async () => {
-  await testHelpers.initDb();
+  await testHelpers.emptyDb();
 }, TIMEOUT);
+
 describe("restaurants", () => {
-  test("should get all restaurants", async () => {
-    const res = await api.get("/restaurants");
+  test(
+    "can create new restaurant",
+    async () => {
+      const user = {
+        name: "test",
+        email: "test@gmail.com",
+        password: "abc12345",
+        role: "business",
+      };
+      const filePath = path.join(__dirname, "pic.png");
 
-    expect(res.status).toBe(200);
-    expect(res.body.length).toBe(1);
-    expect(res.body[0]).toHaveProperty("name");
-    expect(res.body[0]).toHaveProperty("description");
-    expect(res.body[0]).toHaveProperty("address");
-    expect(res.body[0]).toHaveProperty("menuItems");
-  });
+      await testHelpers.userCreation(api, user);
+      const login = await testHelpers.userLogin(api, {
+        email: user.email,
+        password: user.password,
+      });
 
-  test("can add new restaurant", async () => {
-    const filePath = path.join(__dirname, "pic.png");
-    const res = await api
-      .post("/restaurants")
-      .field("name", "test")
-      .field("description", "test")
-      .field("address", "test")
-      .attach("logo", filePath);
+      const token = login.body.token;
+      const res = await api
+        .post("/restaurants")
+        .set("Authorization", `Bearer ${token}`)
+        .field("name", "test")
+        .field("description", "test")
+        .field("address", "test")
+        .attach("logo", filePath, { filename: "testLogo.png" });
 
-    expect(res.status).toBe(201);
-    expect(res.body).toHaveProperty("name");
-    expect(res.body).toHaveProperty("description");
-    expect(res.body).toHaveProperty("address");
-    expect(res.body).not.toHaveProperty("owner");
-    expect(res.body).toHaveProperty("menuItems");
-    expect(res.body).toHaveProperty("image");
-    expect(res.body.image).toBe("/images/logo.png");
-  });
+      console.log({ res: res.body });
+
+      expect(res.status).toBe(201);
+      expect(res.body).toHaveProperty("name");
+      expect(res.body).toHaveProperty("description");
+      expect(res.body.logo).not.toHaveLength(0);
+    },
+    TIMEOUT
+  );
 });
 
 describe("menu", () => {
