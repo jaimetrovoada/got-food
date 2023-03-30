d453808671d5ec9687abe42daf658135b0ed0bfc
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-30 15:44:29 +0800

    add useLocalStorage hook

--- client/hooks/index.ts
+++ client/hooks/index.ts
@@ -28,3 +28,17 @@ export const useToasts = () => {
 
   return { message, type, setSuccessMsg, setErrorMsg, setInfoMsg };
 };
+
+export const useLocalStorage = <T>(key: string, fallbackValue: T) => {
+  const [value, setValue] = useState(fallbackValue);
+  useEffect(() => {
+    const stored = localStorage.getItem(key);
+    setValue(stored ? JSON.parse(stored) : fallbackValue);
+  }, [fallbackValue, key]);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as const;
+}
