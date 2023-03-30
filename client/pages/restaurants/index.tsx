a530da0174bd6f1c86bcb4d396da8347021bcfa3
jaimetrovoada <jaimetrovoada@gmail.com>
2023-03-30 09:46:36 +0800

    create restaurants route

---
+++ client/pages/restaurants/index.tsx
@@ -0,0 +1,35 @@
+import restaurantsService from "@/services/restaurantsService";
+import Image from "next/image";
+import React from "react";
+
+const Restaurants = () => {
+  const { restaurants, isLoading, isError } =
+    restaurantsService.useRestaurants();
+
+  console.log({ restaurants });
+  if (isLoading) {
+    return <div>loading</div>;
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      {restaurants?.map((restaurant) => (
+        <div key={restaurant.id} className="flex flex-row rounded-2xl border">
+          <img
+            src={restaurant.logo}
+            alt={restaurant.name + " " + "logo"}
+            width={50}
+            height={50}
+            className="rounded-tl-2xl rounded-bl-2xl"
+          />
+          <div className="p-2">
+            <h3 className="text-2xl font-bold">{restaurant.name}</h3>
+            <p className="text-gray-500">{restaurant.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Restaurants;
