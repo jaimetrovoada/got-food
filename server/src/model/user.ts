d896582a53156597dcd305c688b091b3bfe608d7
jaimetrovoada <jaimetrovoada@gmail.com>
2023-04-07 14:06:26 +0800

    set email property as unique

--- server/src/model/user.ts
+++ server/src/model/user.ts
@@ -12,7 +12,7 @@ export interface IUser {
 
 const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   role: { type: String, required: true },
   restaurants: [
