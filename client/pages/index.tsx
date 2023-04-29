445bb8e85d21adecd9130f464f222a19e458d511
jaimetrovoada <jaimetrovoada@gmail.com>
2023-04-29 18:09:03 +0800

    add home page styles

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex h-full items-center justify-center">
       <Button as={Link} href={"/restaurants"}>
         Explore
       </Button>
-    </>
+    </div>
   );
 }
