7c0f771aa54ceca25c1b2b80ed665554e5f1d6dc
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-01 10:35:19 +0800

    add getLayout to _app

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,13 +3,25 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { store } from "@/reducers/store";
 import { Provider } from "react-redux";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
-  );
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout =
+    Component.getLayout ??
+    ((page) => (
+      <Provider store={store}>
+        <Layout>{page}</Layout>
+      </Provider>
+    ));
+
+  return getLayout(<Component {...pageProps} />);
 }
