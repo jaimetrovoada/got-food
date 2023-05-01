e3c8ae68c25828f42b4525e0619202d4023ed90b
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-01 10:34:32 +0800

    add services for userDetails and userOrders

--- client/services/userService.ts
+++ client/services/userService.ts
@@ -6,6 +6,52 @@ interface IUserRestaurants {
   id: string;
   name: string;
 }
+interface IUserDetails {
+  id: string;
+  name: string;
+  email: string;
+  restaurants: IUserRestaurants[];
+  role: "customer" | "business";
+}
+
+interface IOrder {
+  restaurant: string;
+  orderedItems: OrderedItem[];
+  totalPrice: number;
+  date: Date;
+  id: string;
+}
+
+interface OrderedItem {
+  item: Item;
+  amount: number;
+  _id: string;
+}
+
+interface Item {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  image: string;
+  restaurant: string;
+  id: string;
+}
+
+const useUserDetails = (id: string) => {
+  const fetcher = (url: string) =>
+    axios.get<IUserDetails>(url).then((res) => res.data);
+
+  const { data, isLoading, error } = useSWR(
+    `${config.BACKEND_URL}/api/users/${id}`,
+    fetcher
+  );
+  return {
+    user: data,
+    isLoading,
+    error,
+  };
+};
 
 const useUserRestaurants = (id: string) => {
   const fetcher = (url: string) =>
@@ -23,4 +69,19 @@ const useUserRestaurants = (id: string) => {
   };
 };
 
-export default { useUserRestaurants };
+const useUserOrders = (id: string) => {
+  const fetcher = (url: string) =>
+    axios.get<IOrder[]>(url).then((res) => res.data);
+
+  const { data, isLoading, error } = useSWR(
+    `${config.BACKEND_URL}/api/users/${id}/orders`,
+    fetcher
+  );
+  return {
+    orders: data,
+    isLoading,
+    error,
+  };
+};
+
+export default { useUserDetails, useUserRestaurants, useUserOrders };
