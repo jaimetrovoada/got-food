3cf98c6b71a172011eb2503c836a6f496c78c342
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-01 11:17:43 +0800

    create user orders page

---
+++ client/pages/users/[dynamicSlug]/orders/index.tsx
@@ -0,0 +1,84 @@
+import Button from "@/components/Button";
+import { getUserLayout } from "@/components/UserLayout";
+import { NextPageWithLayout } from "@/pages/_app";
+import { RootState } from "@/reducers/store";
+import userService, { IOrder } from "@/services/userService";
+import React from "react";
+import { useSelector } from "react-redux";
+
+interface Props {
+  order: IOrder;
+}
+
+const OrderCard = ({ order }: Props) => {
+  const [isExpanded, setIsExpanded] = React.useState(false);
+
+  const date = new Date(order.date);
+  const dateString = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+  const time = `${date.getHours()}:${date.getMinutes()}`;
+
+  return (
+    <div className="flex flex-col border-2 border-black p-2 shadow-custom">
+      <div className="flex justify-between">
+        <span className="font-bold">{order.restaurant.name}</span>
+        <span className="font-bold">${order.totalPrice}</span>
+      </div>
+      <span className="text-gray-500">
+        {dateString} - {time}
+      </span>
+      <div
+        className={`${
+          isExpanded ? "max-h-screen" : "max-h-0"
+        } overflow-hidden transition-all`}
+      >
+        <ul className="list-inside list-disc">
+          {order.orderedItems.map((item) => (
+            <li key={item.item.image} className="list-item text-gray-700">
+              {item.item.name} - ${item.item.price} x {item.amount}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Button
+        className="mx-auto text-gray-500"
+        kind="tertiary"
+        onClick={() => setIsExpanded((prev) => !prev)}
+      >
+        Details
+      </Button>
+    </div>
+  );
+};
+
+const OrdersPage: NextPageWithLayout = () => {
+  const user = useSelector((state: RootState) => state.user);
+  const { orders, isLoading, error } = userService.useUserOrders(user.id);
+  console.log({ orders });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (!orders) {
+    return <>No orders made yet</>;
+  }
+
+  return (
+    <div>
+      Order History:
+      <div className="mx-auto max-w-screen-md">
+        {orders.map((order) => (
+          <OrderCard key={order.id} order={order} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+OrdersPage.getLayout = getUserLayout;
+
+export default OrdersPage;
