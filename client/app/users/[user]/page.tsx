2db212a1f7baf564c7cdaf0a0ad9cda41e4fb556
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-08 17:48:17 +0800

    add form to user profile page

--- client/app/users/[user]/page.tsx
+++ client/app/users/[user]/page.tsx
@@ -3,25 +3,75 @@
 import React from "react";
 import { RootState } from "@/reducers/store";
 import { useSelector } from "react-redux";
-import { useUserDetails } from "@/hooks";
+import { useToasts, useUserDetails } from "@/hooks";
+import { useParams } from "next/navigation";
+import Form from "@/components/Forms/Form";
+import { useInput } from "@/hooks";
+import authService from "@/services/authService";
 
 const UserPage = () => {
   const user = useSelector((state: RootState) => state.user);
   const { user: userinfo } = useUserDetails(user.id);
+  const slug = useParams().user;
+  console.log({ slug });
+  if (!user || user.id !== slug) {
+    throw new Error("NotLoggedIn");
+  }
+
+  const [nameValue, handleNameChange] = useInput(user.name);
+  const [emailValue, handleEmailChange] = useInput(user.email);
+  const [passwordValue, handlePasswordChange] = useInput(null);
+  const { setSuccessMsg, setErrorMsg } = useToasts();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await authService.updateUser(user.token, user.id, {
+        name: nameValue,
+        email: emailValue,
+        password: passwordValue,
+        role: user.role,
+      });
+      if (res.status === 200) {
+        setSuccessMsg("User Updated");
+      }
+      console.log({ res });
+    } catch (err) {
+      console.log(err);
+      setErrorMsg(err.message);
+    }
+  };
 
   return (
-    <>
-      <p className="text-3xl font-bold">Hi {user?.name}</p>
-      <p>{userinfo?.name}</p>
-      <p>{userinfo?.email}</p>
-      <p>{userinfo?.role}</p>
-      <p>Restaurants:</p>
-      <ul>
-        {userinfo?.restaurants.map((restaurant) => (
-          <li key={restaurant.id}>{restaurant.name}</li>
-        ))}
-      </ul>
-    </>
+    <section className="mx-auto w-full max-w-screen-md">
+      <p className="mb-4 text-3xl font-bold">Hi {user.name}</p>
+      <Form handleSubmit={handleSubmit}>
+        <Form.Input
+          name="name"
+          id="name"
+          type="text"
+          handleChange={handleNameChange}
+          value={nameValue}
+          disabled
+        />
+        <Form.Input
+          name="email"
+          id="email"
+          type="text"
+          handleChange={handleEmailChange}
+          value={emailValue}
+          disabled
+        />
+        <Form.Input
+          name="password"
+          id="password"
+          type="password"
+          handleChange={handlePasswordChange}
+          value={passwordValue}
+          disabled
+        />
+      </Form>
+    </section>
   );
 };
 
