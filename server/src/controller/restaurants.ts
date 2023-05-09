406e20944a449126df849c7b6f540e3a13f4ebd8
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-09 13:09:30 +0800

    create functions for updating order and menu iterm details

--- server/src/controller/restaurants.ts
+++ server/src/controller/restaurants.ts
@@ -5,6 +5,7 @@ import middleware from "../utils/middleware";
 import { uploadToFirebase } from "../lib/helpers";
 import { MenuItem, Order, Restaurant, priceSchema } from "../lib/schemas";
 import { IOrder } from "../model/order";
+import { IMenu } from "../model/menu";
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -229,26 +230,97 @@ router.put(
   async (req, res) => {
     const { user } = req;
     try {
+      const restaurant = await models.Restaurant.findById(req.params.id);
+      const logo = req.file ? await uploadToFirebase(req) : restaurant.logo;
       const vRestaurant = Restaurant.parse({
         name: req.body.name,
         description: req.body.description,
         address: req.body.address,
         owner: user._id,
         menuItems: req.body.menuItems,
+        logo: logo,
       });
 
-      const restaurant = await models.Restaurant.findByIdAndUpdate(
-        req.params.id,
-        vRestaurant
-      );
+      restaurant.name = vRestaurant.name;
+      restaurant.description = vRestaurant.description;
+      restaurant.address = vRestaurant.address;
+      restaurant.logo = vRestaurant.logo;
 
-      res.json(restaurant);
+      const update = await restaurant.save();
+
+      res.json(update);
     } catch (err) {
       res.status(500).json({ message: err.message });
       console.log({ err });
     }
   }
 );
-export default router;
 
-// TODO: allow multiple file upload
+router.put(
+  "/:id/menu/:menuId",
+  upload.single("image"),
+  async (req, res, next) => {
+    const { id, menuId } = req.params;
+    try {
+      const restaurant = await models.Restaurant.findById(id).populate<{
+        menuItems: IMenu[];
+      }>("menuItems");
+      const menuItem = await models.Menu.findById(menuId);
+
+      const imgUrl = req.file ? await uploadToFirebase(req) : menuItem.image;
+
+      const vMenuItem = MenuItem.parse({
+        name: req.body.name,
+        description: req.body.description,
+        price: priceSchema.parse(req.body.price),
+        category: req.body.category,
+        image: imgUrl,
+      });
+
+      menuItem.name = vMenuItem.name;
+      menuItem.description = vMenuItem.description;
+      menuItem.price = vMenuItem.price;
+      menuItem.category = vMenuItem.category;
+      menuItem.image = vMenuItem.image;
+      const updatedMenuItem = await menuItem.save();
+      const restMenuItem = restaurant.menuItems.find(
+        (m) => m._id.toString() === menuId
+      );
+      restMenuItem.name = updatedMenuItem.name;
+      restMenuItem.description = updatedMenuItem.description;
+      restMenuItem.price = updatedMenuItem.price;
+      restMenuItem.category = updatedMenuItem.category;
+      restMenuItem.image = updatedMenuItem.image;
+
+      await restaurant.save();
+
+      res.json(updatedMenuItem);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.put("/:id/order/:orderId", async (req, res, next) => {
+  const { orderId, id } = req.params;
+  try {
+    const order = await models.Order.findById(orderId);
+    const restaurant = await models.Restaurant.findById(id).populate<{
+      orders: IOrder[];
+    }>("orders");
+    const restOrder = restaurant.orders.find(
+      (o) => o._id.toString() === orderId
+    );
+
+    const vOrderStatus = Order.shape.status.parse(req.body.status);
+    order.status = vOrderStatus;
+    const updatedOrder = await order.save();
+    restOrder.status = vOrderStatus;
+    await restaurant.save();
+    res.json(updatedOrder);
+  } catch (err) {
+    console.log({ err });
+    next(err);
+  }
+});
+export default router;
