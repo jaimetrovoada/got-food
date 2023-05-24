3a97243c11405a002b4440ce3df83f77f1b1e7c1
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-24 16:24:11 +0800

    update form handlers, add loding cards

--- client/app/users/[user]/restaurants/page.tsx
+++ client/app/users/[user]/restaurants/page.tsx
@@ -1,96 +1,77 @@
 "use client";
 
-import { LinkCard } from "@/components/Card";
+import { CardSkeleton, LinkCard } from "@/components/Card";
 import RestaurantForm from "@/components/Forms/RestaurantForm";
-import { useToasts } from "@/hooks";
+import { useFileInput, useInput, useToasts } from "@/hooks";
 import { RootState } from "@/reducers/store";
 import restaurantsService from "@/services/restaurantsService";
 import { useUserRestaurants } from "@/hooks";
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
-
-interface FormData {
-  name: string;
-  description: string;
-  address: string;
-  logo: File | undefined;
-}
 
 const UserRestaurantPage = () => {
   const user = useSelector((state: RootState) => state.user);
 
-  const { restaurants, isLoading, error } = useUserRestaurants(user.id);
+  const { restaurants, isLoading } = useUserRestaurants(user.id);
+  console.log({ restaurants });
+
+  const [nameValue, nameInput] = useInput("");
+  const [descriptionValue, descriptionInput] = useInput("");
+  const [addressValue, addressInput] = useInput("");
+  const [imageValue, imageInput] = useFileInput();
 
   const { setSuccessMsg, setErrorMsg } = useToasts();
-  const [formState, setFormState] = useState<FormData>({
-    name: "",
-    description: "",
-    address: "",
-    logo: undefined,
-  });
+
   const formHandlers = {
     handleSubmit: async (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
       console.log("handleSubmit");
       try {
-        const res = await restaurantsService.createRestaurant(
-          user.token,
-          formState
-        );
+        const res = await restaurantsService.createRestaurant(user.token, {
+          name: nameValue,
+          description: descriptionValue,
+          address: addressValue,
+          logo: imageValue,
+        });
         console.log({ res });
         if (res.status === 201) {
           setSuccessMsg("Restaurant created");
-          setFormState({
-            name: "",
-            description: "",
-            address: "",
-            logo: undefined,
-          });
         }
       } catch (err) {
         console.log({ err });
         setErrorMsg("something went wrong");
       }
     },
-    handleNameInput: (e: React.ChangeEvent<HTMLInputElement>) => {
-      setFormState({ ...formState, name: e.target.value });
-    },
-    handleDescInput: (e: React.ChangeEvent<HTMLInputElement>) => {
-      setFormState({ ...formState, description: e.target.value });
-    },
-    handleAddrInput: (e: React.ChangeEvent<HTMLInputElement>) => {
-      setFormState({ ...formState, address: e.target.value });
-    },
-    handleLogoUpload: (e: React.ChangeEvent<HTMLInputElement>) => {
-      console.log({ e });
-      const file = e.target.files?.[0];
-      console.log({ files: e.target.files });
-      if (file) {
-        setFormState({ ...formState, logo: file });
-      }
-    },
+    handleNameInput: nameInput,
+    handleDescInput: descriptionInput,
+    handleAddrInput: addressInput,
+    handleLogoUpload: imageInput,
   };
-  if (isLoading) {
-    return <div>loading...</div>;
-  }
-  if (error) {
-    console.log({ error });
-    return <div>something went wrong</div>;
-  }
+
   return (
     <section className="mx-auto w-full max-w-screen-md">
-      {user?.role === "business" ? <RestaurantForm {...formHandlers} /> : null}
+      <RestaurantForm {...formHandlers} />
       <div className="flex flex-col gap-4">
         <h3>your restaurants</h3>
 
         <div className="flex flex-col gap-4">
-          {restaurants?.map((restaurant) => (
-            <LinkCard
-              href={`/users/${user.id}/restaurants/${restaurant.id}/details`}
-              key={restaurant.id}
-              name={restaurant.name}
-            />
-          ))}
+          {isLoading ? (
+            <>
+              <CardSkeleton />
+              <CardSkeleton />
+              <CardSkeleton />
+            </>
+          ) : (
+            restaurants.map((restaurant) => (
+              <LinkCard
+                href={`/users/${user.id}/restaurants/${restaurant.id}/details`}
+                key={restaurant.id}
+                name={restaurant.name}
+                description={restaurant.description}
+                imageUrl={restaurant.logo}
+              />
+            ))
+          )}
         </div>
       </div>
     </section>
