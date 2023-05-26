a9f79361c1ad476cb3d5a85364ded188d685ce27
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-26 12:27:04 +0800

    rework restaurant menu dashboard

--- client/app/manage/[slug]/menu/page.tsx
+++ client/app/manage/[slug]/menu/page.tsx
@@ -1,21 +1,18 @@
 "use client";
 
-import { ItemCard } from "@/components/Card";
+import Button from "@/components/Button";
+import Card from "@/components/Card";
 import MenuForm from "@/components/Forms/MenuForm";
-import { useRestaurantMenu, useToasts } from "@/hooks";
+import Modal, { ModalHandler } from "@/components/Modal";
+import { useFileInput, useInput, useRestaurantMenu, useToasts } from "@/hooks";
 import { RootState } from "@/reducers/store";
 import restaurantsService from "@/services/restaurantsService";
 import { useParams } from "next/navigation";
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
-
-interface FormData {
-  name: string;
-  description: string;
-  price: number;
-  category: string;
-  image: File | undefined;
-}
+import Image from "next/image";
+import { IMenuItem } from "@/types";
+import CardWrapper from "@/components/CardWrapper";
 
 const Page = () => {
   const { setSuccessMsg, setErrorMsg } = useToasts();
@@ -27,64 +24,44 @@ const Page = () => {
 
   const { menu } = useRestaurantMenu(slug);
 
-  const [formData, setFormData] = useState<FormData>({
-    name: "",
-    description: "",
-    price: 0,
-    category: "",
-    image: undefined,
-  });
+  const [nameValue, handleNameChange, setNameValue] = useInput("");
+  const [descriptionValue, handleDescriptionChange, setDescriptionValue] =
+    useInput("");
+  const [priceValue, handlePriceChange, setPriceValue] = useInput("");
+  const [categoryValue, handleCategoryChange, setCategoryValue] = useInput("");
+  const [imageValue, handleImageChange] = useFileInput();
 
   const formHandlers = {
     handleSubmit: async (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
       console.log("handleSubmit");
       try {
-        const res = await restaurantsService.addMenuItem(
-          user.token,
-          slug,
-          formData
-        );
+        const res = await restaurantsService.addMenuItem(user.token, slug, {
+          name: nameValue,
+          description: descriptionValue,
+          price: Number(priceValue),
+          category: categoryValue,
+          image: imageValue,
+        });
         console.log({ res });
         if (res.status === 201) {
           setSuccessMsg("Item added");
-
         }
       } catch (err) {
         console.log({ err });
         setErrorMsg("something went wrong");
       }
     },
-    handleNameChange: (event: React.ChangeEvent<HTMLInputElement>) => {
-      setFormData({
-        ...formData,
-        name: event.target.value,
-      });
-    },
-    handleDescriptionChange: (event: React.ChangeEvent<HTMLInputElement>) => {
-      setFormData({
-        ...formData,
-        description: event.target.value,
-      });
-    },
-    handlePriceChange: (event: React.ChangeEvent<HTMLInputElement>) => {
-      setFormData({
-        ...formData,
-        price: Number(event.target.value),
-      });
-    },
-    handleImageChange: (event: React.ChangeEvent<HTMLInputElement>) => {
-      setFormData({
-        ...formData,
-        image: event.target.files?.[0],
-      });
-    },
-    handleCategoryChange: (event: React.ChangeEvent<HTMLInputElement>) => {
-      setFormData({
-        ...formData,
-        category: event.target.value,
-      });
-    },
+    handleNameChange,
+    handleDescriptionChange,
+    handlePriceChange,
+    handleImageChange,
+    handleCategoryChange,
+    nameValue,
+    descriptionValue,
+    priceValue,
+    imageValue,
+    categoryValue,
   };
 
   const handleDeleteItem = async (id: string) => {
@@ -100,24 +77,105 @@ const Page = () => {
     }
   };
 
+  const modalRef = useRef<ModalHandler>(null);
+
+  const editModalRef = useRef<ModalHandler>(null);
+
+  const handleEditItem = (item: IMenuItem) => {
+    setNameValue(item.name);
+    setDescriptionValue(item.description);
+    setPriceValue(item.price.toString());
+    setCategoryValue(item.category);
+    editModalRef.current?.show();
+  };
+
+  const editFormHandlers = {
+    handleSubmit: async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      console.log("handleSubmit");
+    },
+    handleNameChange,
+    handleDescriptionChange,
+    handlePriceChange,
+    handleImageChange,
+    handleCategoryChange,
+    nameValue,
+    descriptionValue,
+    priceValue,
+    imageValue,
+    categoryValue,
+  };
+
   return (
-    <section className="mx-auto w-full max-w-screen-md">
-      <MenuForm {...formHandlers} />
-      <div>
-        {menu?.map((item) => (
-          <ItemCard
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            description={item.description}
-            imageUrl={item.image}
-            price={item.price}
-            deleteItem={handleDeleteItem}
-          />
-        ))}
-      </div>
+    <section className="mx-auto w-full max-w-screen-md overflow-hidden pb-10">
+      <Modal ref={modalRef}>
+        <MenuForm {...formHandlers} />
+      </Modal>
+      <Modal ref={editModalRef}>
+        <MenuForm {...editFormHandlers} />
+      </Modal>
+      <CardWrapper>
+        <Button
+          variant="custom"
+          className="w-full rounded-lg bg-blue-200/50 p-4 font-bold text-blue-600"
+          onClick={() => modalRef.current?.show()}
+        >
+          Add item
+        </Button>
+        <div className="flex flex-1 flex-col gap-2 overflow-y-auto scrollbar-hide">
+          {menu?.map((item) => (
+            <MenuCard
+              key={item.id}
+              item={item}
+              deleteItem={handleDeleteItem}
+              editItem={handleEditItem}
+            />
+          ))}
+        </div>
+      </CardWrapper>
     </section>
   );
 };
 
 export default Page;
+interface MenuCardProps {
+  item: IMenuItem;
+  deleteItem: (id: string) => void;
+  editItem: (item: IMenuItem) => void;
+}
+const MenuCard = ({ item, editItem, deleteItem }: MenuCardProps) => {
+  return (
+    <Card className="flex flex-row items-center justify-between border-black/10 shadow-sm">
+      <div className="flex flex-row gap-2 rounded-l-2xl">
+        <Image
+          src={item.image}
+          alt={`image of the dish - ${item.name}`}
+          width={50}
+          height={50}
+          className="h-auto w-auto rounded-l-2xl object-cover"
+        />
+        <div className="flex flex-col gap-2 p-2">
+          <p className="text-xl font-bold">{item.name}</p>
+          <p className="text-gray-500">{item.description}</p>
+        </div>
+      </div>
+      <div className="p-2">
+        <p className="text-lg font-bold">${item.price}</p>
+        <Button
+          variant="custom"
+          onClick={() => editItem(item)}
+          className="h-8 w-8 leading-none"
+        >
+          ✒️
+        </Button>
+        <Button
+          variant="custom"
+          onClick={(e) => deleteItem(item.id)}
+          className="h-8 w-8 leading-none"
+        >
+          🗑️
+        </Button>
+      </div>
+    </Card>
+  );
+};
