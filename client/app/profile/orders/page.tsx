6ac382d5db6f50def8c6d1a08b5da38ac6842e5b
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-27 13:57:37 +0800

    make profile routes SSR

--- client/app/profile/orders/page.tsx
+++ client/app/profile/orders/page.tsx
@@ -1,60 +1,20 @@
-"use client";
-
-import Button from "@/components/Button";
-import { RootState } from "@/reducers/store";
-import { useUserOrders } from "@/hooks";
 import React from "react";
-import { useSelector } from "react-redux";
 import { IOrder } from "@/types";
-import Card from "@/components/Card";
+import { OrderCard } from "@/components/Card";
+import axios from "axios";
+import { getUser } from "@/lib/auth";
+import config from "@/utils/config";
 
-interface Props {
-  order: IOrder;
-}
-
-const OrderCard = ({ order }: Props) => {
-  const [isExpanded, setIsExpanded] = React.useState(false);
-
-  const date = new Date(order.date);
-  const dateString = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
-  const time = `${date.getHours()}:${date.getMinutes()}`;
-
-  return (
-    <Card className="flex flex-col p-2" variant="square">
-      <div className="flex justify-between">
-        <span className="font-bold">{order.restaurant.name}</span>
-        <span className="font-bold">${order.totalPrice}</span>
-      </div>
-      <span className="text-gray-500">
-        {dateString} - {time}
-      </span>
-      <div
-        className={`${
-          isExpanded ? "max-h-screen" : "max-h-0"
-        } overflow-hidden transition-all`}
-      >
-        <ul className="list-inside list-disc">
-          {order.orderedItems.map((item) => (
-            <li key={item.item.image} className="list-item text-gray-700">
-              {item.item.name} - ${item.item.price} x {item.amount}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Button
-        className="mx-auto text-gray-500"
-        variant="tertiary"
-        onClick={() => setIsExpanded((prev) => !prev)}
-      >
-        Details
-      </Button>
-    </Card>
+const getUserOrders = async () => {
+  const user = await getUser();
+  const res = await axios.get<IOrder[]>(
+    `${config.BACKEND_URL}/api/users/${user.id}/orders`
   );
+  return res.data;
 };
 
-const OrdersPage = () => {
-  const user = useSelector((state: RootState) => state.user);
-  const { orders } = useUserOrders(user.id);
+const OrdersPage = async () => {
+  const orders = await getUserOrders();
   console.log({ orders });
 
   if (!orders || orders.length === 0) {
@@ -64,7 +24,7 @@ const OrdersPage = () => {
   return (
     <section className="mx-auto w-full max-w-screen-md">
       <p>Order History:</p>
-      <div>
+      <div className="flex flex-col gap-2">
         {orders.map((order) => (
           <OrderCard key={order.id} order={order} />
         ))}
--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,72 +1,14 @@
-"use client";
-
 import React from "react";
-import { RootState } from "@/reducers/store";
-import { useSelector } from "react-redux";
-import { useToasts } from "@/hooks";
-import Form from "@/components/Forms/Form";
-import { useInput } from "@/hooks";
-import authService from "@/services/authService";
+import { getUser } from "@/lib/auth";
+import ClientForm from "./client-page";
 
-const UserPage = () => {
-  const user = useSelector((state: RootState) => state.user);
-
-  const [nameValue, handleNameChange] = useInput(user.name);
-  const [emailValue, handleEmailChange] = useInput(user.email);
-  const [passwordValue, handlePasswordChange] = useInput(null);
-  const { setSuccessMsg, setErrorMsg } = useToasts();
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const res = await authService.updateUser(user.token, user.id, {
-        name: nameValue,
-        email: emailValue,
-        password: passwordValue,
-        role: user.role,
-      });
-      if (res.status === 200) {
-        setSuccessMsg("User Updated");
-      }
-      console.log({ res });
-    } catch (err) {
-      console.log(err);
-      setErrorMsg(err.message);
-    }
-  };
+const UserPage = async () => {
+  const user = await getUser();
 
   return (
     <section className="mx-auto w-full max-w-screen-md">
       <p className="mb-4 text-3xl font-bold">Hi {user.name}</p>
-      <Form onSubmit={handleSubmit}>
-        <Form.Input
-          name="name"
-          id="name"
-          type="text"
-          onChange={handleNameChange}
-          labelText="Name"
-          value={nameValue}
-          disabled
-        />
-        <Form.Input
-          name="email"
-          id="email"
-          type="text"
-          onChange={handleEmailChange}
-          labelText="Email"
-          value={emailValue}
-          disabled
-        />
-        <Form.Input
-          name="password"
-          id="password"
-          type="password"
-          onChange={handlePasswordChange}
-          labelText="Password"
-          value={passwordValue}
-          disabled
-        />
-      </Form>
+      <ClientForm user={user} />
     </section>
   );
 };
