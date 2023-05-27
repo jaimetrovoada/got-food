e6480eb4384811512dca8e03949fb659ff097e29
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-27 15:21:08 +0800

    make restaurant details page SSR

---
+++ client/app/manage/[slug]/details/client-page.tsx
@@ -0,0 +1,80 @@
+"use client";
+import Button from "@/components/Button";
+import Form from "@/components/Forms/Form";
+import { useInput, useToasts } from "@/lib/hooks";
+import restaurantsService from "@/lib/restaurantsService";
+import { IRestaurant, IUser } from "@/types";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  restaurant: IRestaurant;
+  user: IUser;
+}
+
+const RestaurantDetailsForm = ({ restaurant, user }: Props) => {
+  const [nameValue, handleNameChange] = useInput(restaurant?.name);
+  const [descriptionValue, handleDescriptionChange] = useInput(
+    restaurant?.description
+  );
+  const [addressValue, handleAddressChange] = useInput(restaurant?.address);
+
+  const router = useRouter();
+  const { setSuccessMsg, setErrorMsg } = useToasts();
+
+  const handleDeleteRestaurant = async () => {
+    try {
+      const res = await restaurantsService.deleteRestaurant(
+        user.token,
+        restaurant?.id
+      );
+
+      if (res.status === 200) {
+        setSuccessMsg("Restaurant deleted successfully");
+        router.replace(`/users/${user.id}/restaurants`);
+      }
+      console.log({ res });
+    } catch (error) {
+      console.log({ delError: error });
+      setErrorMsg("Something went wrong");
+    }
+  };
+
+  return (
+    <>
+      <Form onSubmit={null}>
+        <Form.Input
+          name="name"
+          id="name"
+          type="text"
+          onChange={handleNameChange}
+          labelText="Name"
+          value={nameValue}
+          disabled
+        />
+        <Form.Input
+          name="description"
+          id="description"
+          type="text"
+          onChange={handleDescriptionChange}
+          labelText="Description"
+          value={descriptionValue}
+          disabled
+        />
+        <Form.Input
+          name="address"
+          id="address"
+          type="text"
+          onChange={handleAddressChange}
+          labelText="Address"
+          value={addressValue}
+          disabled
+        />
+        <Button onClick={handleDeleteRestaurant} className="bg-red-500">
+          🗑️
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default RestaurantDetailsForm;
--- client/app/manage/[slug]/details/page.tsx
+++ client/app/manage/[slug]/details/page.tsx
@@ -1,50 +1,29 @@
-"use client";
-
-import Button from "@/components/Button";
-import { useRestaurant, useToasts } from "@/lib/hooks";
-import { RootState } from "@/lib/reducers/store";
+import { getUser } from "@/lib/auth";
 import restaurantsService from "@/lib/restaurantsService";
-import { useParams, useRouter } from "next/navigation";
 import React from "react";
-import { useSelector } from "react-redux";
+import RestaurantDetailsForm from "./client-page";
 
-const Page = () => {
-  const params = useParams();
-  const slug = params?.slug as string;
-
-  const router = useRouter();
-
-  const { setErrorMsg, setSuccessMsg } = useToasts();
-
-  const { restaurant } = useRestaurant(slug);
-  const user = useSelector((state: RootState) => state.user);
-
-  const handleDeleteRestaurant = async () => {
-    try {
-      const res = await restaurantsService.deleteRestaurant(
-        user.token,
-        restaurant?.id
-      );
-
-      if (res.status === 200) {
-        setSuccessMsg("Restaurant deleted successfully");
-        router.replace(`/users/${user.id}/restaurants`);
-      }
-      console.log({ res });
-    } catch (error) {
-      console.log({ delError: error });
-      setErrorMsg("Something went wrong");
-    }
+interface Props {
+  params: {
+    slug: string;
   };
+}
+
+async function getRestaurant(slug: string) {
+  const { restaurant } = await restaurantsService.getRestaurant(slug);
+
+  return restaurant;
+}
+
+const Page = async ({ params }: Props) => {
+  const slug = params?.slug as string;
+  const user = await getUser();
+
+  const restaurant = await getRestaurant(slug);
 
   return (
     <section className="mx-auto w-full max-w-screen-md">
-      <h1>{restaurant?.name}</h1>
-      <p>{restaurant?.description}</p>
-      <p>{restaurant?.address}</p>
-      <Button onClick={handleDeleteRestaurant} className="bg-red-500">
-        🗑️
-      </Button>
+      <RestaurantDetailsForm restaurant={restaurant} user={user} />
     </section>
   );
 };
