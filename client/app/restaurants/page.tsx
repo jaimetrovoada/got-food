f7f98257cb7765f693260a65bf8bd070b5391d0b
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-27 17:54:13 +0800

    move trending slider to restaurants page

--- client/app/restaurants/page.tsx
+++ client/app/restaurants/page.tsx
@@ -1,13 +1,25 @@
 import Container from "@/components/Container";
 import RestaurantList from "@/components/RestaurantList";
+import TrendingSlider from "@/components/TrendingSlider";
 import restaurantsService from "@/lib/restaurantsService";
 import React from "react";
 
 const Page = async () => {
   const { restaurants } = await getRestaurants();
   return (
-    <Container className="flex flex-col gap-4 p-2">
-      <RestaurantList restaurants={restaurants} />
+    <Container className="flex flex-col gap-8 p-2">
+      <div>
+        <p className="mb-4 text-3xl font-bold capitalize">
+          what&apos;s trending
+        </p>
+        <TrendingSlider trending={restaurants} />
+      </div>
+      <div>
+        <p className="mb-4 text-3xl font-bold">All</p>
+        <section className="grid grid-flow-row grid-cols-1 gap-4 p-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <RestaurantList restaurants={restaurants} />
+        </section>
+      </div>
     </Container>
   );
 };
