5d4ed5ebf53482b09bc8dfc10a6a4a82453f3f78
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-27 13:56:39 +0800

    extract user orders card

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
-import { IMenuItem } from "@/types";
+import { IMenuItem, IOrder } from "@/types";
 
 interface ItemCardProps {
   id: string;
@@ -187,6 +189,49 @@ export const MenuItemCard = ({ item, addToCart }: MenuCardProps) => {
           +
         </Button>
       </div>
+    </Card>
+  );
+};
+
+interface OrderCardProps {
+  order: IOrder;
+}
+export const OrderCard = ({ order }: OrderCardProps) => {
+  const [isExpanded, setIsExpanded] = React.useState(false);
+
+  const date = new Date(order.date);
+  const dateString = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+  const time = `${date.getHours()}:${date.getMinutes()}`;
+
+  return (
+    <Card className="flex flex-col p-2" variant="square">
+      <div className="flex justify-between">
+        <span className="font-bold">{order.restaurant.name}</span>
+        <span className="font-bold">${order.totalPrice}</span>
+      </div>
+      <span className="text-gray-500">
+        {dateString} - {time}
+      </span>
+      <div
+        className={`${
+          isExpanded ? "max-h-screen" : "max-h-0"
+        } overflow-hidden transition-all`}
+      >
+        <ul className="list-inside list-disc">
+          {order.orderedItems.map((item) => (
+            <li key={item.item.image} className="list-item text-gray-700">
+              {item.item.name} - ${item.item.price} x {item.amount}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Button
+        className="mx-auto text-gray-500"
+        variant="tertiary"
+        onClick={() => setIsExpanded((prev) => !prev)}
+      >
+        Details
+      </Button>
     </Card>
   );
 };
