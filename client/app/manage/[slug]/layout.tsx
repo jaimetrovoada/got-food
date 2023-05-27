29cf52449b59369fddc087d27e2484eb712d7272
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-27 13:32:41 +0800

    remove auth check

--- client/app/manage/[slug]/layout.tsx
+++ client/app/manage/[slug]/layout.tsx
@@ -1,16 +1,9 @@
-"use client";
 import ManageLayout from "@/components/ManageLayout";
-import { RootState } from "@/reducers/store";
-import { useSelector } from "react-redux";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: Props) {
-  const user = useSelector((state: RootState) => state.user);
-  if (!user || !user.id) {
-    throw new Error("NotLoggedIn");
-  }
   return <ManageLayout>{children}</ManageLayout>;
 }
--- client/app/manage/layout.tsx
+++ client/app/manage/layout.tsx
@@ -1,23 +1,8 @@
-"use client";
-
 import Container from "@/components/Container";
-import { useSession } from "next-auth/react";
-
 interface Props {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: Props) {
-  const { status } = useSession({
-    required: true,
-    onUnauthenticated() {
-      throw new Error("NotLoggedIn");
-      // The user is not authenticated, handle it here.
-    },
-  });
-  if (status === "loading") {
-    return <div>Loading</div>;
-  }
-
   return <Container className="overflow-y-hidden">{children}</Container>;
 }
