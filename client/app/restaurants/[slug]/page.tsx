9e3fe06c50ffd5be241099919b502b2bbbd68260
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-27 16:02:28 +0800

    make restaurant page SSR

--- client/app/restaurants/[slug]/page.tsx
+++ client/app/restaurants/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import restaurantsService from "@/lib/restaurantsService";
-import React from "react";
 import Restaurant from "./restaurant_page";
 import { Metadata } from "next";
 import { getUser } from "@/lib/auth";
@@ -18,25 +17,12 @@ const Page = async ({ params }: Props) => {
 
 export default Page;
 
-export async function generateStaticParams() {
-  const { restaurants } = await restaurantsService.getRestaurants();
-
-  const paths = restaurants.map((restaurant) => {
-    return { slug: restaurant.id };
-  });
-
-  return paths;
-}
-
 async function getRestaurant(params: { slug: string }) {
   const { menu } = await restaurantsService.getMenu(params.slug);
   const { restaurant } = await restaurantsService.getRestaurant(params.slug);
 
   return { menu, restaurant };
 }
-
-export const dynamicParams = true,
-  revalidate = 60;
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const restaurant = await getRestaurant(params);
