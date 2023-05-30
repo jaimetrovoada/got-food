aba0a34f185d586e7156e4409548fc65e5af7403
jaimetrovoada <jaimetrovoada@gmail.com>
2023-05-30 15:20:31 +0800

    add zIndex to toasts

--- client/components/Toasts.tsx
+++ client/components/Toasts.tsx
@@ -7,7 +7,7 @@ const Toasts = () => {
 
   return (
     <div
-      className={`absolute right-4 top-4 rounded-2xl p-4
+      className={`absolute right-4 top-4 z-50 rounded-2xl p-4
       ${type === "success" && "bg-green-500"} 
       ${type === "info" && "bg-yellow-500"} 
       ${type === "error" && "bg-red-500"} 
