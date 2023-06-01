3a81e2977a819e700f7ca02ec113e1231a6096b5
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-01 13:13:00 +0800

    change login form to use react-hook-form

--- client/app/auth/login/page.tsx
+++ client/app/auth/login/page.tsx
@@ -1,36 +1,7 @@
-"use client";
 import LoginForm from "@/components/Forms/LoginForm";
-import { useInput, useToasts } from "@/lib/hooks";
-import { signIn } from "next-auth/react";
-import { useSearchParams } from "next/navigation";
 
 const Page = () => {
-  const { setSuccessMsg, setErrorMsg } = useToasts();
-  const [email, setEmail] = useInput("");
-  const [password, setPassword] = useInput("");
-
-  const searchParams = useSearchParams();
-
-  const loginFormHandlers = {
-    handleSubmit: async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      const res = await signIn("credentials", {
-        email,
-        password,
-        redirect: true,
-        callbackUrl: searchParams.get("callbackUrl") || "/",
-      });
-
-      if (res.ok) {
-        setSuccessMsg("Logged in");
-      } else {
-        setErrorMsg("something went wrong");
-      }
-    },
-    handleEmailInput: setEmail,
-    handlePasswordInput: setPassword,
-  };
-  return <LoginForm {...loginFormHandlers} />;
+  return <LoginForm />;
 };
 
 export default Page;
--- client/components/Forms/LoginForm.tsx
+++ client/components/Forms/LoginForm.tsx
@@ -1,35 +1,68 @@
-import React from "react";
-import Form from "./Form";
+"use client";
 import Button from "../Button";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { useToasts } from "@/lib/hooks";
 
-interface LoginFormProps {
-  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-  handleEmailInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handlePasswordInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+interface Props {}
+
+interface Inputs {
+  email: string;
+  password: string;
 }
 
-const LoginForm: React.FC<LoginFormProps> = ({
-  handleEmailInput,
-  handlePasswordInput,
-  handleSubmit,
-}) => {
+const LoginForm = ({}: Props) => {
+  const searchParams = useSearchParams();
+  const { setSuccessMsg, setErrorMsg } = useToasts();
+
+  const { register, handleSubmit, watch, reset } = useForm<Inputs>();
+
+  const email = watch("email");
+  const password = watch("password");
+
+  const onSubmit = async (data: Inputs) => {
+    const res = await signIn("credentials", {
+      email: data.email,
+      password: data.password,
+      redirect: true,
+      callbackUrl: searchParams.get("callbackUrl") || "/",
+    });
+
+    if (res.ok) {
+      setSuccessMsg("Logged in");
+    } else {
+      setErrorMsg("something went wrong");
+    }
+  };
+
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Input
-        type="email"
-        name="email"
-        id="email"
-        onChange={handleEmailInput}
-        labelText="Email"
-      />
-      <Form.Input
-        type="password"
-        name="password"
-        id="password"
-        onChange={handlePasswordInput}
-        labelText="Password"
-      />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-4 rounded-2xl border-2 border-black/50 bg-white p-4 shadow-custom"
+      onReset={() => reset()}
+    >
+      <div className="flex flex-col">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          className="rounded-xl border p-2 focus:outline-none"
+          {...register("email", {
+            required: true,
+          })}
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          className="rounded-xl border p-2 focus:outline-none"
+          {...register("password", { required: true })}
+        />
+      </div>
       <Button
         as={Link}
         href="/auth/register"
@@ -38,7 +71,15 @@ const LoginForm: React.FC<LoginFormProps> = ({
       >
         Create Account
       </Button>
-    </Form>
+      <div className="flex gap-4">
+        <Button type="submit" disabled={!email || !password}>
+          Submit
+        </Button>
+        <Button type="reset" variant="secondary">
+          Reset
+        </Button>
+      </div>
+    </form>
   );
 };
 
