4c075fc116b0b8378969caeca1c29b36572ade95
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-01 14:23:56 +0800

    change register form to use react-hook-form

--- client/components/Forms/RegisterForm.tsx
+++ client/components/Forms/RegisterForm.tsx
@@ -1,88 +1,126 @@
-import React, { useState } from "react";
-import Form from "./Form";
+"use client";
 import Button from "../Button";
+import { useForm } from "react-hook-form";
+import authService from "@/lib/authService";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { useToasts } from "@/lib/hooks";
 
 export enum Role {
   BUSINESS = "business",
   CUSTOMER = "customer",
 }
 
-interface RegisterFormProps {
-  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-  handleNameInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleEmailInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handlePasswordInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleRoleInput: (role: Role) => void;
+interface Inputs {
+  name: string;
+  email: string;
+  password: string;
+  role: Role;
 }
+interface Props {}
 
-const RegisterForm: React.FC<RegisterFormProps> = ({
-  handleSubmit,
-  handleNameInput,
-  handleEmailInput,
-  handlePasswordInput,
-  handleRoleInput,
-}) => {
-  const [role, setRole] = useState<Role | undefined>(undefined);
-  const setRoleBusiness = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setRole(Role.BUSINESS);
-    handleRoleInput(Role.BUSINESS);
-  };
-  const setRoleCustomer = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setRole(Role.CUSTOMER);
-    handleRoleInput(Role.CUSTOMER);
+const RegisterForm = ({}: Props) => {
+  const router = useRouter();
+  const { setSuccessMsg, setErrorMsg } = useToasts();
+
+  const { register, watch, handleSubmit, reset } = useForm<Inputs>({
+    defaultValues: {
+      role: Role.CUSTOMER,
+    },
+  });
+  const name = watch("name");
+  const email = watch("email");
+  const password = watch("password");
+  const role = watch("role");
+
+  console.log({ name, email, password, role });
+
+  const onSubmit = async (data: Inputs) => {
+    try {
+      const res = await authService.register({
+        role: data.role,
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      });
+      console.log({ res });
+      if (res.status === 201) {
+        setSuccessMsg("Registration Complete");
+        router.replace("/auth/login");
+      }
+    } catch (err) {
+      console.log({ err });
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409) {
+          return setErrorMsg(err.response?.data?.error);
+        }
+      }
+      setErrorMsg("Something went wrong, please try again");
+    }
   };
 
-  if (role === undefined) {
-    return (
-      <div className="flex flex-col gap-2">
-        <Button onClick={setRoleBusiness}>Sign up to add my restuarants</Button>
-        <Button onClick={setRoleCustomer}>Create a costumer account</Button>
-      </div>
-    );
-  }
   return (
-    <Form onSubmit={handleSubmit}>
-      {role ? (
-        <>
-          <Form.Input
-            name="name"
-            id="name"
-            onChange={handleNameInput}
-            labelText="Name"
-          />
-          <Form.Input
-            type="email"
-            name="email"
-            id="email"
-            onChange={handleEmailInput}
-            labelText="Email"
-          />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-4 rounded-2xl border-2 border-black/50 bg-white p-4 shadow-custom"
+    >
+      <div className="flex flex-col">
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          className="rounded-xl border p-2 focus:outline-none"
+          {...register("name", { required: true })}
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          className="rounded-xl border p-2 focus:outline-none"
+          {...register("email", { required: true })}
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          className="rounded-xl border p-2 focus:outline-none"
+          {...register("password", { required: true })}
+        />
+      </div>
 
-          <Form.Input
-            type="password"
-            name="password"
-            id="password"
-            onChange={handlePasswordInput}
-            labelText="Password"
+      <fieldset className="flex flex-row gap-4">
+        <legend>I want to add my restaurants: </legend>
+        <div>
+          <input
+            type="radio"
+            id="radio_yes"
+            {...register("role", { required: true })}
+            value={Role.BUSINESS}
           />
-        </>
-      ) : (
-        <>
-          <button
-            onClick={setRoleBusiness}
-            className="rounded-xl border p-2 shadow-md"
-          >
-            Sign up to add my restuarants
-          </button>
-          <button
-            onClick={setRoleCustomer}
-            className="rounded-xl border p-2 shadow-md"
-          >
-            Create a costumer account
-          </button>
-        </>
-      )}
-    </Form>
+          <label htmlFor="role">Yes</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="radio_no"
+            {...register("role", { required: true })}
+            value={Role.CUSTOMER}
+          />
+          <label htmlFor="role">No</label>
+        </div>
+      </fieldset>
+      <div className="flex gap-4">
+        <Button type="submit" disabled={!email || !password || !name || !role}>
+          Submit
+        </Button>
+        <Button useResetStyles variant="secondary" onClick={() => reset()}>
+          Reset
+        </Button>
+      </div>
+    </form>
   );
 };
 
