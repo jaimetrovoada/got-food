d2bab68279f58ee6268f2e4cf0706f06643b2c96
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-02 17:20:07 +0800

    handle no data for restaurants page

--- client/app/restaurants/page.tsx
+++ client/app/restaurants/page.tsx
@@ -5,21 +5,35 @@ import restaurantsService from "@/lib/restaurantsService";
 import React from "react";
 
 const Page = async () => {
-  const restaurants = await getRestaurants();
-  const trendingRestaurants = await getGetTrending();
+  const restaurantsData = getRestaurants();
+  const trendingRestaurantsData = getGetTrending();
+  const [restaurants, trendingRestaurants] = await Promise.all([
+    restaurantsData,
+    trendingRestaurantsData,
+  ]);
   return (
     <Container className="flex flex-col gap-8 p-2">
+      {trendingRestaurants ? (
+        <div>
+          <p className="mb-4 text-3xl font-bold capitalize">
+            what&apos;s trending
+          </p>
+          <TrendingSlider trending={trendingRestaurants} />
+        </div>
+      ) : null}
       <div>
-        <p className="mb-4 text-3xl font-bold capitalize">
-          what&apos;s trending
-        </p>
-        <TrendingSlider trending={trendingRestaurants} />
-      </div>
-      <div>
-        <p className="mb-4 text-3xl font-bold">All</p>
-        <section className="grid grid-flow-row grid-cols-1 gap-4 p-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          <RestaurantList restaurants={restaurants} />
-        </section>
+        {restaurants ? (
+          <>
+            <p className="mb-4 text-3xl font-bold">All</p>
+            <section className="grid grid-flow-row grid-cols-1 gap-4 p-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              <RestaurantList restaurants={restaurants} />
+            </section>
+          </>
+        ) : (
+          <>
+            <p>Nothing yet!!! Comeback some other time</p>
+          </>
+        )}
       </div>
     </Container>
   );
@@ -40,7 +54,8 @@ async function getGetTrending() {
   const [restaurant, err] = await restaurantsService.getTrendingRestaurants();
 
   if (err) {
-    throw err;
+    return null;
   }
+
   return restaurant;
 }
