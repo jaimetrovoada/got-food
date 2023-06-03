b9fec18bdd8b77167b902f0e873abf3be4dfe401
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-03 13:15:25 +0800

    modify 404 page styles

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,12 +1,24 @@
+import Button from "@/components/Button";
 import Container from "@/components/Container";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
     <Container className="flex items-center justify-center">
-      <h2 className="text-3xl font-bold">Not Found</h2>
-      <p className="text-lg">
-        The page you&apos;re trying to access doesn&apos;t exist
-      </p>
+      <div className="text-center">
+        <p className="text-base font-semibold text-blue-600">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          Page not found
+        </h1>
+        <p className="mt-6 text-base leading-7 text-gray-600">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Button as={Link} href="/">
+            Go back home
+          </Button>
+        </div>
+      </div>
     </Container>
   );
 }
