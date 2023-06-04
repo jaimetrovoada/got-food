c81a5b8a73735872b033d24d8be469ef92428d5e
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-04 14:50:15 +0800

    add font: Inter

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,14 @@ import "@/styles/globals.css";
 import { Providers } from "./providers";
 import Layout from "@/components/Layout";
 
+import { Inter } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
@@ -10,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         <Providers>
           <Layout>{children}</Layout>
