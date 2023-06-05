e4f0b6cdd1c1d9f8d77db2f53bd821d1ec743ead
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-05 11:49:01 +0800

    create separate header component

---
+++ client/components/Header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import UserDropdown from "./UserDropdown";
+import { IUser } from "@/types";
+
+interface Props {
+  user: IUser;
+}
+
+const Header = ({ user }: Props) => {
+  return (
+    <header className="relative bg-white">
+      <nav className="container mx-auto flex flex-row items-center justify-between py-2 px-4 xl:px-0">
+        <Link href="/" className="text-3xl font-bold uppercase">
+          got food?
+        </Link>
+        <UserDropdown user={user} />
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import Link from "next/link";
+import ToastsProvider from "@/lib/contexts/ToastsProvider";
 import React from "react";
 import Toasts from "./Toasts";
-import ToastsProvider from "@/lib/contexts/ToastsProvider";
-import UserDropdown from "./UserDropdown";
 
 interface Props {
   children: React.ReactNode;
@@ -13,14 +11,6 @@ interface Props {
 const Layout = ({ children }: Props) => {
   return (
     <>
-      <header className="relative bg-white">
-        <nav className="container mx-auto flex flex-row items-center justify-between py-2 px-4 xl:px-0">
-          <Link href="/" className="text-3xl font-bold uppercase">
-            got food?
-          </Link>
-          <UserDropdown />
-        </nav>
-      </header>
       <ToastsProvider>
         <main className="flex flex-1 flex-col overflow-auto scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-gray-300 scrollbar-track-rounded-md scrollbar-thumb-rounded-md">
           {children}
