7bebd107799cd28517dc8469734d13200603c0de
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-21 16:54:24 +0800

    modify cardWrapper component

--- client/components/CardWrapper.tsx
+++ client/components/CardWrapper.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const CardWrapper = ({ children }: Props) => {
   return (
-    <div className="flex max-h-full flex-col gap-2 overflow-hidden rounded-2xl border border-gray-200 bg-white p-2 shadow-lg md:p-8">
+    <div className="flex max-h-full flex-col gap-2 overflow-hidden rounded-2xl border border-gray-200 border-gray-600/50 bg-neutral-950 p-2 shadow-lg md:p-8">
       {children}
     </div>
   );
