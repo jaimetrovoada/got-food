f7f1acf81435b5b642b818f4b29b2f6d7b29212b
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-22 15:21:56 +0800

    add div props to card wrapper

--- client/components/CardWrapper.tsx
+++ client/components/CardWrapper.tsx
@@ -1,10 +1,18 @@
-interface Props {
+import { getClasses } from "@/lib/helpers";
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
 }
 
-const CardWrapper = ({ children }: Props) => {
+const CardWrapper = ({ children, className, ...props }: Props) => {
   return (
-    <div className="flex max-h-full flex-col gap-2 overflow-hidden rounded-2xl border border-gray-200 border-gray-600/50 bg-neutral-950 p-2 shadow-lg md:p-8">
+    <div
+      className={getClasses(
+        "flex max-h-full flex-col gap-2 overflow-hidden rounded-2xl border border-gray-200 border-gray-600/50 bg-neutral-950 p-2 shadow-lg md:p-8",
+        className
+      )}
+      {...props}
+    >
       {children}
     </div>
   );
