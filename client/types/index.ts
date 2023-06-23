58ac91780585ead0947bcb8175d40a01cab8a8d8
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-23 13:57:54 +0800

    change type definitions

--- client/types/index.ts
+++ client/types/index.ts
@@ -7,65 +7,6 @@ import {
   restaurantRequestValidator,
 } from "@/lib/shemas";
 
-export interface IUserRestaurants {
-  id: string;
-  name: string;
-  logo: string;
-  description: string;
-}
-
-export interface IUser {
-  name: string;
-  email: string;
-  role: "customer" | "business";
-  restaurants: string[];
-  orders: string[];
-  id: string;
-  token: string;
-}
-
-export interface IOrder {
-  restaurant: IUserRestaurants;
-  orderedItems: IOrderedItem[];
-  totalPrice: number;
-  date: Date;
-  id: string;
-  status: string;
-  tableNumber: string;
-  orderId: string;
-}
-
-export interface IOrderedItem {
-  item: string;
-  amount: number;
-}
-
-export interface IRestaurant {
-  id: string;
-  name: string;
-  description: string;
-  address: string;
-  menuItems: IMenuItem[];
-  owner: {
-    name: string;
-    email: string;
-  };
-  logo: string;
-}
-
-export interface IMenuItem {
-  id: string;
-  name: string;
-  description: string;
-  price: number;
-  category: string;
-  image: string;
-}
-export enum UserRole {
-  CUSTOMER = "customer",
-  BUSINESS = "business",
-}
-
 export type RegisterRequest = z.infer<typeof registerRequestValidator>;
 
 export type LoginRequest = z.infer<typeof loginRequestValidator>;
@@ -74,27 +15,38 @@ export type LoginResponse = {
   id: string;
   name: string;
   email: string;
-  role: string;
+  role: z.infer<typeof registerRequestValidator.shape.role>;
   token: string;
+};
+
+export type IUser = LoginResponse & {
+  restaurants: IRestaurant[];
+  orders: IOrder[];
 };
 
 export type RestaurantRequest = z.infer<typeof restaurantRequestValidator>;
 
-export type RestaurantResponse = RestaurantRequest & {
+export type IRestaurant = RestaurantRequest & {
   id: string;
+  menu: IMenuItem[];
+  orders: IOrder[];
 };
 
 export type MenuRequest = z.infer<typeof menuRequestValidator>;
 
-export type MenuResponse = MenuRequest & {
+export type IMenuItem = MenuRequest & {
   id: string;
 };
 
 export type OrderRequest = z.infer<typeof orderRequestValidator>;
 
-export type OrderResponse = OrderRequest & {
+export type IOrder = OrderRequest & {
   id: string;
   user: string;
   restaurant: string;
   date: Date;
+  orderedItems: {
+    item: string;
+    amount: number;
+  }[];
 };
