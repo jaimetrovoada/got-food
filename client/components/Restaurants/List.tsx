ffb162d387ad64d5d91c944d165c0cd1fb22f725
jaimetrovoada <jaimetrovoada@gmail.com>
2023-07-11 16:45:12 +0800

    remove suspense loader

--- client/components/Restaurants/List.tsx
+++ client/components/Restaurants/List.tsx
@@ -1,7 +1,5 @@
 import { IRestaurant } from "@/types";
-import { Suspense } from "react";
 import Item from "./Item";
-import Loader from "./Loader";
 
 interface Props {
   restaurants: IRestaurant[];
@@ -10,26 +8,16 @@ interface Props {
 const List = ({ restaurants }: Props) => {
   return (
     <section className="grid flex-1 grid-cols-1 gap-4 p-2 md:grid-cols-2 lg:grid-cols-3">
-      <Suspense
-        fallback={
-          <>
-            <Loader />
-            <Loader />
-            <Loader />
-          </>
-        }
-      >
-        {restaurants.map((restaurant) => (
-          <Item
-            href={`/restaurants/${restaurant.id}`}
-            key={restaurant.id}
-            name={restaurant.name}
-            imageUrl={restaurant.logo}
-            description={restaurant.description}
-            address={restaurant.address}
-          />
-        ))}
-      </Suspense>
+      {restaurants.map((restaurant) => (
+        <Item
+          href={`/restaurants/${restaurant.id}`}
+          key={restaurant.id}
+          name={restaurant.name}
+          imageUrl={restaurant.logo}
+          description={restaurant.description}
+          address={restaurant.address}
+        />
+      ))}
     </section>
   );
 };
