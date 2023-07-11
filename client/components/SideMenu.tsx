00b87f75c5731b52ca7bbb0b3c1a8831071ce12f
jaimetrovoada <jaimetrovoada@gmail.com>
2023-07-11 15:16:11 +0800

    modify sidebar

--- client/components/SideMenu.tsx
+++ client/components/SideMenu.tsx
@@ -1,70 +1,120 @@
 import { IRestaurant, LoginResponse } from "@/types";
 import Button from "./Button";
 import Link from "next/link";
-import { signOut } from "next-auth/react";
+import { signOut, signIn } from "next-auth/react";
 import Avatar from "./Avatar";
 import { ChevronDown } from "react-feather";
 import { getClasses } from "@/lib/helpers";
+import { usePathname } from "next/navigation";
 
 interface Props {
-  user?: LoginResponse;
-  restaurants?: IRestaurant[];
+  user: LoginResponse | undefined;
+  restaurants: IRestaurant[] | undefined;
   isOpen: boolean;
 }
 
 const SideMenu = ({ user, restaurants, isOpen }: Props) => {
-  const menuList =
-    user?.role === "business"
-      ? [
-          {
-            name: "Explore",
-            link: `/restaurants`,
-          },
-          {
-            name: "Profile",
-            link: `/profile`,
-          },
-          {
-            name: "My Restaurants",
-            link: `/manage`,
-            submenu: restaurants?.map((restaurant) => {
-              return {
-                name: restaurant.name,
-                link: `/manage/${restaurant.id}`,
-              };
-            }),
-          },
-        ]
-      : [
-          {
-            name: "Explore",
-            link: `/restaurants`,
-          },
-          {
-            name: "Profile",
-            link: `/users/${user?.id}`,
-          },
-        ];
+  const menuList = [
+    {
+      name: "Explore",
+      link: `/restaurants`,
+    },
+    {
+      name: "Profile",
+      link: `/profile`,
+    },
+  ];
+
+  const myRestaurantsList = restaurants?.map((restaurant) => {
+    return {
+      name: restaurant.name,
+      id: restaurant.id,
+      link: `/manage/${restaurant.id}/details`,
+    };
+  });
+
+  const currentRoute = usePathname();
+  const isActive = (route: string) => {
+    return currentRoute === route;
+  };
+
+  const isRestaurantActive = (id: string) => {
+    return (
+      currentRoute === `/manage/${id}/details` ||
+      currentRoute === `/manage/${id}/orders` ||
+      currentRoute === `/manage/${id}/menu`
+    );
+  };
+
   return (
     <div
-      className={getClasses("flex w-auto flex-col gap-1 p-4 lg:w-auto", {
-        "hidden w-0": !isOpen,
-      })}
+      className={getClasses(
+        "flex w-auto flex-col gap-1 p-4 lg:flex lg:w-auto",
+        {
+          "hidden w-0": !isOpen,
+        }
+      )}
     >
       <div className="flex max-h-full flex-1 flex-col gap-1 overflow-auto">
         {menuList.map((item) => (
-          <Item key={item.name} {...item} />
+          <Item
+            key={item.name}
+            name={item.name}
+            link={item.link}
+            isActive={isActive(item.link)}
+          />
         ))}
+        {user && user.role === "business" && (
+          <>
+            <details className="group [&_summary::-webkit-details-marker]:hidden">
+              <summary
+                className={getClasses(
+                  "flex cursor-pointer items-center justify-between gap-1.5 rounded-lg px-4 py-2 hover:bg-neutral-950/40",
+                  {
+                    "bg-neutral-950 text-gray-200 hover:bg-neutral-950":
+                      isActive("/manage"),
+                  }
+                )}
+              >
+                <Button
+                  as={Link}
+                  href={"/manage"}
+                  variant="custom"
+                  className="font-semibold text-slate-200"
+                >
+                  My Restaurants
+                </Button>
+                <ChevronDown className="transition duration-300 group-open:-rotate-180" />
+              </summary>
+              {myRestaurantsList.map((item) => (
+                <div className="flex-flex-col gap-1 p-2" key={item.name}>
+                  <Item
+                    key={item.name}
+                    name={item.name}
+                    link={item.link}
+                    isActive={isRestaurantActive(item.id)}
+                  />
+                </div>
+              ))}
+            </details>
+          </>
+        )}
       </div>
       <div className="flex flex-row items-center gap-2 ">
-        <Button
-          onClick={() => signOut()}
-          variant="custom"
-          className="inline-flex flex-row items-center gap-2 rounded-xl border border-transparent p-2 font-semibold text-slate-200 hover:border-neutral-200/75"
-        >
-          <Avatar name={user?.name} />
-          <span>Logout</span>
-        </Button>
+        {user ? (
+          <Button
+            onClick={() => signOut()}
+            variant="custom"
+            className="inline-flex flex-row items-center gap-2 rounded-xl border border-transparent p-2 font-semibold text-slate-200 hover:border-neutral-200/75"
+          >
+            <Avatar name={user?.name} />
+            <span>Logout</span>
+          </Button>
+        ) : (
+          <Button onClick={() => signIn()} variant="secondary">
+            Login
+          </Button>
+        )}
       </div>
     </div>
   );
@@ -75,44 +125,21 @@ export default SideMenu;
 interface ItemProps {
   name: string;
   link: string;
-  submenu?: ItemProps[];
+  isActive: boolean;
 }
-const Item = ({ name, link, submenu }: ItemProps) => {
-  if (submenu?.length) {
-    return (
-      <details className="group [&_summary::-webkit-details-marker]:hidden">
-        <summary className="flex cursor-pointer items-center justify-between gap-1.5 rounded-xl border border-transparent p-2 hover:border-neutral-200/75">
-          <Button
-            as={Link}
-            href={link}
-            variant="custom"
-            className="font-semibold text-slate-200 "
-          >
-            {name}
-          </Button>
-          <ChevronDown className="transition duration-300 group-open:-rotate-180" />
-        </summary>
-        {submenu.map((subitem) => (
-          <div className="flex-flex-col gap-1 p-2" key={subitem.name}>
-            <Button
-              as={Link}
-              href={subitem.link}
-              variant="custom"
-              className="w-full rounded-xl border border-transparent p-2 text-slate-200 hover:border-neutral-200/75"
-            >
-              {subitem.name}
-            </Button>
-          </div>
-        ))}
-      </details>
-    );
-  }
+const Item = ({ name, link, isActive }: ItemProps) => {
   return (
     <Button
       as={Link}
       href={link}
       variant="custom"
-      className="w-full rounded-xl border border-transparent p-2 font-semibold text-slate-200 hover:border-neutral-200/75"
+      className={getClasses(
+        "w-full rounded-lg px-4 py-2 font-semibold text-slate-200 hover:bg-neutral-950/40 ",
+        {
+          "bg-neutral-950 px-4 py-2 text-gray-200 hover:bg-neutral-950":
+            isActive,
+        }
+      )}
     >
       {name}
     </Button>
