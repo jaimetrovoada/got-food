4de6f0352846576878fde38414ce089e9a0c9dcc
jaimetrovoada <jaimetrovoada@gmail.com>
2023-12-11 14:21:58 +0000

    change imported global css

--- client/app/(app)/layout.tsx
+++ client/app/(app)/layout.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@/app/globals.css";
 import { Providers } from "./providers";
 import { Inter } from "next/font/google";
 import { getUser } from "@/lib/auth.service";
